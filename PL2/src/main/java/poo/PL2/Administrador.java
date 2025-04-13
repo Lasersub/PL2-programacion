2162f29bd50a467e0043cc2ad381fbc4e4709dd5
User <User@DESKTOP-EDJ8P3E>
2025-04-13 14:16:14 +0200

    Subclase administrador vacia

---
+++ PL2/src/main/java/poo/PL2/Administrador.java
@@ -0,0 +1,10 @@
+
+package poo.PL2;
+
+class Administrador extends Usuario{
+    
+    public Administrador(String correo, String contrasena) {
+        super(correo, contrasena);
+    }
+        
+}
