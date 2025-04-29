7f5f9fd1e7777f392fd181ebaac0e4b961f67fa7
User <User@DESKTOP-EDJ8P3E>
2025-04-29 12:00:29 +0200

    Creado clase administrador

---
+++ PL2/src/main/java/poo/PL2/Administrador.java
@@ -0,0 +1,33 @@
+
+package poo.PL2;
+
+import java.io.Serializable;
+
+public class Administrador extends Usuario implements Serializable {
+
+    private static final String ADMIN_EMAIL = "admin@javaevents.com";
+    private static final String ADMIN_PASSWORD = "admin";
+
+    public Administrador() {
+        super(ADMIN_EMAIL, ADMIN_PASSWORD);
+    }
+
+    public static boolean esAdmin(String correo, String contrasena) {
+        return ADMIN_EMAIL.equals(correo) && ADMIN_PASSWORD.equals(contrasena);
+    }
+
+    // Métodos exclusivos del administrador
+    public void gestionarEventos() {
+        // implementar
+    }
+
+    public void consultarUsuarios() {
+        // implementar
+    }
+
+    public void consultarReservas() {
+        // implementar
+    }
+
+   
+}
