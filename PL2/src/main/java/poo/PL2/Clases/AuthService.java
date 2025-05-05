73bdcf86e39c92107fde38c634789b9c8749e9a3
User <User@DESKTOP-EDJ8P3E>
2025-05-05 17:44:36 +0200

    Nueva clase AuthService por la cual la interfaz se comunica con la base de datos para registar clientes o verificar su info de inicio de sesion

---
+++ PL2/src/main/java/poo/PL2/Clases/AuthService.java
@@ -0,0 +1,20 @@
+
+package poo.PL2.Clases;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class AuthService {
+    
+    
+    /*          
+    public boolean registrarUsuario(Cliente cliente) {
+        
+    }
+
+    public Usuario iniciarSesion(String email, String contrasena) {
+        
+    }
+    */
+}
