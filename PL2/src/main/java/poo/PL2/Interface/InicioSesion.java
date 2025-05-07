64461e8e61dc00bbabe2cedf788f8f03cc027085
oscar <oscar.lopezrojo@edu.uah.es>
2025-05-07 22:25:08 +0200

    Arreglado botón VOLVER de iniciar sesión

--- PL2/src/main/java/poo/PL2/Interface/InicioSesion.java
+++ PL2/src/main/java/poo/PL2/Interface/InicioSesion.java
@@ -135,7 +135,7 @@ public class InicioSesion extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        Navegacion.cambiarVentana(this, new NuevaCuenta()); // Volver
+        Navegacion.cambiarVentana(this, new MainMenu()); // Volver
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
