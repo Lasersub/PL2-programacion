ad6af50d5d068f6745858b1c81c6e956b49666c3
oscar <oscar.lopezrojo@edu.uah.es>
2025-05-07 21:42:09 +0200

    Arreglado boton de acceder

--- PL2/src/main/java/poo/PL2/Interface/InicioSesion.java
+++ PL2/src/main/java/poo/PL2/Interface/InicioSesion.java
@@ -140,7 +140,7 @@ public class InicioSesion extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-        Navegacion.cambiarVentana(this, new NuevaCuenta()); // Acceder
+        Navegacion.cambiarVentana(this, new PortalCliente()); // Acceder
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
