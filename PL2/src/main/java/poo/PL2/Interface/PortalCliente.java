d5312fed80a1da8f2018d8d7cf4369b1e2527b7f
User <User@DESKTOP-EDJ8P3E>
2025-05-08 00:41:17 +0200

    Solo hecha conexion con NuevaCuentaTarjetaCredito. Cuando se crea una cuenta te redirige al portal de cliente

--- PL2/src/main/java/poo/PL2/Interface/PortalCliente.java
+++ PL2/src/main/java/poo/PL2/Interface/PortalCliente.java
@@ -3,15 +3,20 @@ package poo.PL2.Interface;
 
 import javax.swing.*;
 import java.awt.event.*;
+import poo.PL2.Clases.Cliente;
+
 
 public class PortalCliente extends javax.swing.JFrame {
-
+    
+    private final Cliente cliente;
+    
     /**
      * Creates new form PortalCliente
      */
-    public PortalCliente() {
+    public PortalCliente(Cliente cliente) {
         initComponents();
         this.setLocationRelativeTo(null); // Centra la ventana
+        this.cliente = cliente;
     }
 
     /**
@@ -120,42 +125,6 @@ public class PortalCliente extends javax.swing.JFrame {
         this.dispose(); // cerrar este menú
     }//GEN-LAST:event_jButton3ActionPerformed
 
-
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(PortalCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(PortalCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(PortalCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(PortalCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new PortalCliente().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
