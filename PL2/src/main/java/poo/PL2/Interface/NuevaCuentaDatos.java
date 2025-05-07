1bf4229585b1c1e8f50055298a984bc17014b70f
User <User@DESKTOP-EDJ8P3E>
2025-05-08 00:40:16 +0200

    Hecha conexion con backEnd

--- PL2/src/main/java/poo/PL2/Interface/NuevaCuentaDatos.java
+++ PL2/src/main/java/poo/PL2/Interface/NuevaCuentaDatos.java
@@ -1,25 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+
 package poo.PL2.Interface;
 
 import poo.PL2.Clases.Navegacion;
+import poo.PL2.Clases.RegistroTemporal;
 import poo.PL2.Clases.SesionErrorHandler;
 import poo.PL2.Clases.SesionErrorHandler.ErrorTipo;
-import static poo.PL2.Clases.SesionErrorHandler.ErrorTipo.CAMPO_OBLIGATORIO_VACIO;
 
 /**
  *
  * @author oscar
  */
 public class NuevaCuentaDatos extends javax.swing.JFrame {
+    
+    private final RegistroTemporal registroTemp;
 
     /**
      * Creates new form NuevaCuentaDatos
      */
-    public NuevaCuentaDatos() {
+    public NuevaCuentaDatos(RegistroTemporal registroTemp) {
         initComponents();
+        this.registroTemp = registroTemp;
         this.setLocationRelativeTo(null); // Centra la ventana 
 
     }
@@ -133,7 +133,7 @@ public class NuevaCuentaDatos extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        Navegacion.cambiarVentana(this, new NuevaCuenta()); // Volver
+        Navegacion.cambiarVentana(this, new NuevaCuenta(registroTemp)); // Volver
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
@@ -142,7 +142,10 @@ public class NuevaCuentaDatos extends javax.swing.JFrame {
         (jFormattedTextField1.getText().equals(""))) {
         SesionErrorHandler.mostrarError(ErrorTipo.CAMPO_OBLIGATORIO_VACIO);
     } else {
-        Navegacion.cambiarVentana(this, new NuevaCuentaDireccion()); // Siguiente
+        registroTemp.setNombre(jTextField1.getText());
+        registroTemp.setTelefono(jFormattedTextField1.getText());
+        
+        Navegacion.cambiarVentana(this, new NuevaCuentaDireccion(registroTemp)); // Siguiente
     }
     }//GEN-LAST:event_jButton2ActionPerformed
 
@@ -153,37 +156,7 @@ public class NuevaCuentaDatos extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NuevaCuentaDatos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NuevaCuentaDatos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NuevaCuentaDatos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NuevaCuentaDatos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new NuevaCuentaDatos().setVisible(true);
-            }
-        });
-    }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
