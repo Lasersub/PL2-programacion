5b0f992d6f0c828d9a78066b41977024a4b3eda2
User <User@DESKTOP-EDJ8P3E>
2025-05-21 16:03:31 +0200

    Administrador tiene acceso limitado a database para crear eventos y registrarlos

--- PL2/src/main/java/poo/PL2/Clases/Administrador.java
+++ PL2/src/main/java/poo/PL2/Clases/Administrador.java
@@ -1,31 +1,37 @@
 
 package poo.PL2.Clases;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 
 public class Administrador extends Usuario {
 
     private static final String ADMIN_EMAIL = "admin@javaevents.com";
     private static final String ADMIN_PASSWORD = "admin";
+    private DataBase dataBase;
 
     public Administrador() {
         super(ADMIN_EMAIL, ADMIN_PASSWORD);
+        this.dataBase = DataBase.getInstance();
     }
 
     public static boolean esAdmin(String correo, String contrasena) {
         return ADMIN_EMAIL.equals(correo) && ADMIN_PASSWORD.equals(contrasena);
     }
 
-    // Métodos exclusivos del administrador
-    public void gestionarEventos() {
-        // implementar
+    // MÉTODOS EXCLUSIVOS DE ADMINISTRADOR
+    //Gestion de eventos
+        
+    public void registrarEvento(String titulo, String tipo, ArrayList<LocalDateTime> fechas, String calle, String numero, String ciudad, String codigoPostal, 
+                        double precio, String rutaPortada){
+        
+        ArrayList<Evento> eventos = dataBase.getEventos();
+        
+        Direccion direccion = new Direccion(calle,numero,ciudad,codigoPostal);
+        Evento evento = new Evento(titulo, tipo, fechas, direccion, precio, rutaPortada);
+              
+        dataBase.guardarEvento(evento);
+        
     }
-
-    public void consultarUsuarios() {
-        // implementar
-    }
-
-    public void consultarReservas() {
-        // implementar
-    }
-
 }
